9be9c9f795f0232bb20d99e1875bf0e550d8085e
lottery248 <leemeifung82@yahoo.com.hk>
2016-01-23 23:25:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'lottery248 dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6Ld0MhYTAAAAAOtFDWeRKUFt1p_2otJl5fJlTJ7n',  // <----- EDIT ME!
   redirect_uri: 'https://lottery248dice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
